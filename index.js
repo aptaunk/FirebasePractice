3cb0ba07a48cdcb3d7e82296861064487e2d3bc4
aptaunk <aptaunk@gmail.com>
2017-05-16 14:36:11 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
+const admin = require("firebase-admin");
+const db = admin.database();
+
+admin.initializeApp(functions.config().firebase);
 
 exports.test = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
      res.send("Hello "+req.body.name);
    });
+});
+
+exports.createVolunteer = functions.auth.user().onCreate(event => {
+  const user = event.data;
+  return db.ref("volunteers").set({
+    user.uid: {
+      hours: 0
+    }
+  }).then(() => {
+    console.log('New Volunteer Created!');
+  });
 });
